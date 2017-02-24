da99e91ce7462d983c099f9c95413f4d41f456b2
Subhodip <subhodip.banerjee@snapdeal.com>
2017-02-24 16:59:59 +0530

    Second Commit with FIrst Screen.

---
+++ GoTalk/Categories/UIViewController+AlertsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+AlertsViewController.swift
+//  GoTalk
+//
+//  Created by Subhodip on 24/02/17.
+//  Copyright © 2017 Subhodip. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    /**
+     * Shows a default alert/info message with an OK button.
+     */
+    func showAlertMessage(_ message: String, okButtonTitle: String = "Ok") -> Void {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: okButtonTitle, style: .default, handler: nil)
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+}
